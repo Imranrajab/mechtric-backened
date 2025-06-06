ee95dfc00ab797c0cf1dbd3271fa0d278cb74fbb
Imran Rajab <imranrajab66@gmail.com>
2025-06-06 01:51:52 -0700

    metric

--- controller/formcontroller.js
+++ controller/formcontroller.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const mongo = require("mongoose")
 mongo.connect("mongodb://127.0.0.1:27017/test").catch((e) => console.log("not connected"));
+const Metric = require('../modal/formmodal'); 
+
 exports.getmetric = async (req, res) => {
-  const result = await metric.find({});
+  const result = await Metric.find({});
   res.send({ msg: "Data retrieved", value: result });
 };
 
 exports.postmetric = async (req, res) => {
-  const newData = new metric(req.body);
+  const newData = new Metric(req.body);
   const result = await newData.save();
   res.send({ msg: "Data saved", value: result });
 };
