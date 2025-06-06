a0e1b089398d75033f8f85865bccb166f40d9cad
Imran Rajab <imranrajab66@gmail.com>
2025-06-05 21:53:55 -0700

    fourth

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const metricRouter = require('./router/formrouter');
 const cors = require('cors');
 // ✅ Use dynamic port and bind to all IPs (NOT localhost)
 const PORT = process.env.PORT || 4008;
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
 const options = {
   origin: '*', 
 };
